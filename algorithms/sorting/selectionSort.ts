bd4491426564f47d6e2b81d6abe8681c212a55d4
apella1 <johncollons5@gmail.com>
2023-08-07 11:23:28 +0300

    Implement selection sort

---
+++ algorithms/sorting/selectionSort.ts
@@ -0,0 +1,29 @@
+function selectionSort(array: number[]) {
+  const arrayLength = array.length;
+  for (
+    let lastUnsortedIndex = arrayLength - 1;
+    lastUnsortedIndex > 0;
+    lastUnsortedIndex--
+  ) {
+    let largestNumberIndex = 0;
+    for (let j = 1; j <= lastUnsortedIndex; j++) {
+      if (array[j] > array[largestNumberIndex]) {
+        largestNumberIndex = j;
+      }
+    }
+    swap(array, largestNumberIndex, lastUnsortedIndex);
+  }
+  return array;
+}
+
+function swap(array: number[], a: number, b: number) {
+  let temp = array[a];
+  array[a] = array[b];
+  array[b] = temp;
+}
+
+const unsortedNumbers = [3, 53, -2, 1, 343, 89, 10];
+const sortedNumbers = selectionSort([...unsortedNumbers]);
+
+console.log(unsortedNumbers);
+console.log(sortedNumbers);
